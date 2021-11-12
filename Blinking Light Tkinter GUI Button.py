3f65aa90f4c50c290d58faa1d1833152bc138555
CodebyKhayla <44632276+CodebyKhayla@users.noreply.github.com>
2021-11-12 18:16:07 -0500

    upload project code
    Project: Build a circuit using a Raspberry Pi and Tkinter where an LED blinks when a GUI button is NOT pressed. However, the LED stays on continually when the GUI button IS pressed.

---
+++ Blinking Light Tkinter GUI Button.py
@@ -0,0 +1,56 @@
+#import necessary libraries
+from time import sleep
+import RPi.GPIO as GPIO
+import tkinter as tk
+from tkinter import ttk
+
+#set numbering system and disable warnings
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+#set pin direction
+GPIO.setup(7, GPIO.OUT, initial = 0)
+
+#variable to track when button is pressed or not pressed
+button_on = False 
+
+def blinking():
+    global button_on
+    if button_on == False:
+        GPIO.output(7,0)
+        print('off')
+        sleep(0.5)
+        GPIO.output(7,1)
+        print('on')
+        sleep(0.5)
+        GPIO.output(7,0)
+        print('off')
+        sleep(0.5)
+        GPIO.output(7,1)
+        print('on')
+        sleep(0.5)
+    root.after(1000, blinking)
+
+def stopblinking(event):
+    global button_on
+    button_on = True
+    print(stopblinking)
+    GPIO.output(7,1)
+    return button_on
+
+def button_released(event):
+    global button_on
+    button_on = False 
+    print(button_released)
+    return button_on
+    blinking()
+
+root = tk.Tk()
+root.geometry('400x400')
+btn = ttk.Button(root, text='Light')
+btn.bind('<ButtonPress-1>', stopblinking)
+btn.bind('<ButtonRelease-1>', button_released)
+btn.focus()
+btn.pack(expand=True)
+root.after(1000, blinking)
+root.mainloop()
